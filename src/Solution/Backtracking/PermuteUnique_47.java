8056fecf87a159f76a8d6581a620dbff9b466dcc
Null <borismirage00@gmail.com>
2021-09-22 13:52:13 -0700

    doc: add doc and explanation

--- src/Solution/Backtracking/PermuteUnique_47.java
+++ src/Solution/Backtracking/PermuteUnique_47.java
@@ -24,41 +24,57 @@ public class PermuteUnique_47 {
     /**
      * Backtracking.
      * The difference between permute problem is that one boolean array is added during backtracking process.
+     * To avoid duplicated selection, keep a boolean array to store visited
      * This boolean array is to record if current element is used (for uniqueness).
      *
      * @param nums input number collection
      * @return all possible unique permutations
      */
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums.length == 0) {
-            return res;
+        List<List<Integer>> output = new ArrayList<>();
+
+        if (nums == null || nums.length == 0) {
+            return output;
         }
 
         Arrays.sort(nums);
-        backtracking(nums, new ArrayList<>(), res, new boolean[nums.length]);
-        return res;
+        boolean[] visited = new boolean[nums.length];
+        backtracking(output, new ArrayList<>(), nums, visited);
+
+        return output;
 
     }
 
-    private void backtracking(int[] nums, List<Integer> cache, List<List<Integer>> res, boolean[] use) {
+    /**
+     * Backtracking to find all permutations.
+     * Iterate all the available non-duplicate elements in the array to fill current position in permutations.
+     * Keep a boolean array to store the status of current iteration that whether an element is selected or not.
+     *
+     * @param output  output list
+     * @param temp    list to store each single permutation
+     * @param nums    array to find all permutations
+     * @param visited boolean array to store the status if current element is visited
+     */
+    private void backtracking(List<List<Integer>> output, List<Integer> temp, int[] nums, boolean[] visited) {
 
-        /* Exit point */
-        if (cache.size() == nums.length) {
-            res.add(new ArrayList<>(cache));
-        } else {
-            for (int i = 0; i < nums.length; i++) {
+        if (temp.size() == nums.length) {
+            output.add(new ArrayList<>(temp));
+            return;
+        }
 
-                if (use[i] || i > 0 && nums[i] == nums[i - 1] && !use[i - 1]) {     // avoid reuse element in array
-                    continue;
-                }
-
-                /* Add element to list and mark current element as used */
-                cache.add(nums[i]);
-                use[i] = true;
-                backtracking(nums, cache, res, use);
-                use[i] = false;
-                cache.remove(cache.size() - 1);
+        for (int i = 0; i < nums.length; i++) {
+            /*
+             * Conditions of selection:
+             * 1. Current element is not visited.
+             * 2. Under 1, if:
+             *  a. index is 0, or:
+             *  b. a non-duplicated number, or at the start of new duplicated number sequence */
+            if (!visited[i] && ((i == 0) || (nums[i] != nums[i - 1] || visited[i - 1]))) {
+                temp.add(nums[i]);
+                visited[i] = true;
+                backtracking(output, temp, nums, visited);
+                temp.remove(temp.size() - 1);
+                visited[i] = false;
             }
         }
     }
