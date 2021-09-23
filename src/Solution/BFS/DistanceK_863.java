476e107ca7e890274e77bd254a06d3b35801f494
Null <borismirage00@gmail.com>
2021-09-22 21:39:58 -0700

    fix: correct code

--- src/Solution/BFS/DistanceK_863.java
+++ src/Solution/BFS/DistanceK_863.java
@@ -15,14 +15,14 @@ import java.util.*;
  */
 
 public class DistanceK_863 {
-    private Map<TreeNode, HashSet<TreeNode>> m = new HashMap<>();       // save linked nodes
+    private final Map<TreeNode, TreeNode> map = new HashMap<>();       // save linked nodes
 
     /**
-     * BFS + BFS.
-     * First BFS is to find target, and add link between parent and child node.
-     * Second BFS happens when target node is found.
-     * When target node is found, do BFS starts at target node, utilize the link between nodes to find distance at K.
-     * Note that during the second BFS, the new link between nodes should be added as well.
+     * Option 1: BFS + BFS.
+     * First BFS is to find target, and build children-parent relationship.
+     * Second BFS started from target TreeNode, for each node in each layer, add its non-null child to next level.
+     * Finally, reaches nodes with distance K.
+     * Note that during the second BFS, keep a set to record visited node.
      *
      * @param root   root node
      * @param target target node
@@ -30,107 +30,146 @@ public class DistanceK_863 {
      * @return list of the values of all nodes that have a distance K from the target node
      */
     public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        List<Integer> out = new ArrayList<>();
 
         /* Corner case */
-        if (root == null || K < 1) {
-            if (K == 0) {
-                out.add(target.val);
-                return out;
-            } else {
-                return out;
-            }
+        if (root == null) {
+            return new ArrayList<>();
         }
 
-        Queue<TreeNode> q = new LinkedList<>();
-        m.put(root, new HashSet<>());
-        if (root.left != null) {
-            q.add(root.left);
-        }
-        if (root.right != null) {
-            q.add(root.right);
-        }
-        connect(root.left, root);
-        connect(root.right, root);
-        int size = q.size();
-        boolean canContinue = !(root == target);
-
-        while (!q.isEmpty() && canContinue) {       // BFS to find target and build map
-            for (int i = 0; i < size; i++) {
-                TreeNode tmp = q.poll();
-                if (tmp != null) {
-                    connect(tmp.right, tmp);
-                    connect(tmp.left, tmp);
-                    if (tmp.left != null) {
-                        q.add(tmp.left);
-                    }
-                    if (tmp.right != null) {
-                        q.add(tmp.right);
-                    }
-                    canContinue = !(target.val == tmp.val);
-                }
-            }
+        if (root.val != target.val) {
+            bfs(root, target, map);
         }
 
-        if (!m.containsKey(target)) {
-            return out;
-        }
-
-        Set<TreeNode> s = new HashSet<>();
-        s.add(target);
-        q = new LinkedList<>();     // BFS starts at target node
-        q.add(target);
-
-        while (!q.isEmpty()) {
-            size = q.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode tmp = q.poll();
-                if (tmp != null && m.get(tmp) != null) {
-                    for (TreeNode node : m.get(tmp)) {
-                        if (!s.contains(node)) {
-                            s.add(node);
-                            q.add(node);
-                            if (!m.containsKey(node)) {
-                                m.put(node, new HashSet<>());
-                            }
-
-                            if (node.left != null && !m.containsKey(node)) {
-                                m.get(node).add(node.left);
-                            }
-                            if (node.right != null && !m.containsKey(node)) {
-                                m.get(node).add(node.right);
-                            }
-                        }
-                    }
-                }
-            }
-            K--;
-
-            if (K == 0) {
-                for (TreeNode treeNode : q) {
-                    out.add(treeNode.val);
-                    return out;
-                }
-            }
-        }
-        return out;
+        return bfs(target, map, K);
     }
 
     /**
-     * Add double-way connection between parent and child.
+     * First BFS: build children-parent relationship when finding the target.
      *
-     * @param node   current node
-     * @param parent node of current parent
+     * @param root   root node
+     * @param target target node
+     * @param map    hash map stores children-parent relationship
      */
-    private void connect(TreeNode node, TreeNode parent) {
-        if (node == null) {
-            return;
+    private void bfs(TreeNode root, TreeNode target, Map<TreeNode, TreeNode> map) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean found = false;
+        while (!found) {
+            while (!queue.isEmpty()) {
+                TreeNode current = queue.poll();
+                if (current.left != null) {
+                    map.put(current.left, current);
+                    if (current.left.val == target.val) {
+                        found = true;
+                        break;
+                    } else {
+                        queue.add(current.left);
+                    }
+                }
+                if (current.right != null) {
+                    map.put(current.right, current);
+                    if (current.right.val == target.val) {
+                        found = true;
+                        break;
+                    } else {
+                        queue.add(current.right);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Second BFS to find the nodes with distance k.
+     * Keep a hash set to store the nodes visited to avoid duplicate selection.
+     *
+     * @param target target node
+     * @param map    map to record parent node
+     * @param k      distance K from the target node
+     * @return list of the values of all nodes that have a distance K from the target node
+     */
+    private List<Integer> bfs(TreeNode target, Map<TreeNode, TreeNode> map, int k) {
+        Queue<TreeNode> level = new LinkedList<>();
+        List<Integer> output = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+        level.add(target);
+
+        while (k > 0) {
+            int length = level.size() - 1;
+
+            if (length < 0) { // if k is larger than the furthest level in tree, no node could be returned
+                return new ArrayList<>();
+            }
+
+            while (length >= 0) {
+                TreeNode current = level.poll();
+                visited.add(current.val);
+                if (current.left != null && !visited.contains(current.left.val)) {
+                    level.add(current.left);
+                }
+                if (current.right != null && !visited.contains(current.right.val)) {
+                    level.add(current.right);
+                }
+                if (map.containsKey(current) && !visited.contains(map.get(current).val)) {
+                    level.add(map.get(current));
+                }
+                length--;
+            }
+            k--;
+        }
+        int n = level.size();
+        for (int i = 0; i < n; i++) {
+            output.add(level.poll().val);
         }
 
-        if (!m.containsKey(node)) {
-            m.put(node, new HashSet<>());
-            m.get(node).add(parent);
-            m.get(parent).add(node);
+        return output;
+    }
+
+    /**
+     * Option 2, implement DFS to find target, then implement BFS to find the kth layer of target.
+     * Note that during the BFS process, keep a set to record visited node.
+     * Calling same BFS method with option 1.
+     *
+     * @param root   root node
+     * @param target target node
+     * @param k      distance K from the target node
+     * @return list of the values of all nodes that have a distance K from the target node
+     */
+    public List<Integer> distanceKDFSBFS(TreeNode root, TreeNode target, int k) {
+
+        /* Corner case */
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        if (root.val != target.val) {
+            dfs(root, target, map);
+        }
+
+        return bfs(target, map, k);
+    }
+
+    /**
+     * DFS to find the target and build child-parent relationship, stores in map.
+     *
+     * @param root   root node
+     * @param target target node
+     * @param map    map to record parent node
+     */
+    private void dfs(TreeNode root, TreeNode target, Map<TreeNode, TreeNode> map) {
+        if (root.left != null) {
+            map.put(root.left, root);
+            if (root.left.val == target.val) {
+                return;
+            } else {
+                dfs(root.left, target, map);
+            }
+        }
+        if (root.right != null) {
+            map.put(root.right, root);
+            if (root.right.val != target.val) {
+                dfs(root.right, target, map);
+            }
         }
     }
 
