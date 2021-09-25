dff5d889e926b1e961e82cd32a4fcf84c38a073f
Null <borismirage00@gmail.com>
2021-09-25 16:32:52 -0700

    style: minor revision

--- src/Solution/Others/StrStr_28.java
+++ src/Solution/Others/StrStr_28.java
@@ -13,28 +13,38 @@ package Solution.Others;
  */
 
 public class StrStr_28 {
-
     /**
+     * Simply traverse the haystack and find if haystack.substring(i, i + needle.length()) equals to needle.
+     * Corner cases:
+     * 1. needle > haystack, return -1.
+     * 2. haystack.length() == 0, return -1;
+     * 3. needle.length() == 0, return 0;
+     *
      * @param haystack input string
      * @param needle   input string to be found in haystack
      * @return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack
      */
     public int strStr(String haystack, String needle) {
 
-        /* Corner case */
         if (needle.length() == 0) {
             return 0;
         }
-        if (haystack.length() == 0 || needle.length() > haystack.length()) {
+
+        if (haystack.length() == 0) {
             return -1;
         }
-
-        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            String part = haystack.substring(i, i + needle.length());
-            if (part.equals(needle)) {
-                return i;
+        int length = haystack.length() - needle.length();
+        for (int i = 0; i <= length; i++) {
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == needle.length() - 1) {
+                    return i;
+                }
             }
         }
+
         return -1;
     }
 
