790ca1dfe2303da271985cf5d27f1f9f47077ca9
Null <borismirage00@gmail.com>
2021-09-27 21:44:59 -0700

    style: minor revision

--- src/Solution/Array/MissingNumber_268.java
+++ src/Solution/Array/MissingNumber_268.java
@@ -18,23 +18,52 @@ import java.util.Set;
 
 public class MissingNumber_268 {
     /**
-     * Bit manipulation.
-     * Only one element is missing in 0, 1, 2, ..., n. And XOR operation has a principle a ^ b ^ b = a.
-     * Hence, if array has all elements from 0 to n-1, then the XOR operation will finally back to initial value itself.
-     * i.e, res ^ 1 ^ 1 ^ 2 ^ 2 ... ^ (n - 1) ^ (n - 1) = res.
-     * If the missing int is in range [0, n], then res = res ^ missingIndex ^ n.
-     * Since res initially is equal to n, finally res will be missing index, which is missing number.
+     * Use index mapping to mapping each occurred element from 0 to n by mark each element into negative.
+     * To avoid 0, the mapping should minus 1 to assure each element in array after mapping is negative.
+     * Then check which element in array is not mapped.
      *
      * @param nums input int array
      * @return missing number
      */
     public int missingNumber(int[] nums) {
-
-        int res = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            res = res ^ i ^ nums[i];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0 && (Math.abs(nums[i]) - 1) < n) { // avoid out of bound exception
+                int index = Math.abs(nums[i]) - 1; // index of element to be marked to negative
+                nums[index] = -nums[index] - 1;
+            } else if (nums[i] >= 0 && nums[i] < n) {
+                int index = nums[i]; // index of element to be marked to negative
+                nums[index] = -nums[index] - 1;
+            }
         }
-        return res;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+
+    /**
+     * Bit manipulation.
+     * Only one element is missing in 0, 1, 2, ..., n. And XOR operation has a principle a ^ b ^ b = a.
+     * Hence, if array has all elements from 0 to n-1, then the XOR operation will finally back to initial value itself.
+     * i.e., output ^ 1 ^ 1 ^ 2 ^ 2 ... ^ (n - 1) ^ (n - 1) = output.
+     * If the missing int is in range [0, n], then output = output ^ missingIndex ^ n.
+     * Since output initially is equal to n, finally output will be missing index, which is missing number.
+     *
+     * @param nums input int array
+     * @return missing number
+     */
+    public int missingNumberBitManipulation(int[] nums) {
+
+        int output = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            output = output ^ i ^ nums[i];
+        }
+        return output;
     }
 
     /**
@@ -86,45 +115,11 @@ public class MissingNumber_268 {
         return left;
     }
 
-    /**
-     * Use index mapping to mapping each occurred element from 0 to n by mark each element into negative.
-     * To avoid 0, the mapping should minus 1 to assure each element in array after mapping is negative.
-     * Then check which element in array is not mapped.
-     *
-     * @param nums input int array
-     * @return missing number
-     */
-    public int indexMapping(int[] nums) {
-        int last = 1;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (nums[i] == nums.length || (nums[i] < 0 && Math.abs(nums[i]) - 1 == nums.length)) {
-                last = -1;
-            } else if (nums[i] < 0) {
-                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1] - 1;
-            } else {
-                nums[nums[i]] = -nums[nums[i]] - 1;
-            }
-        }
-
-        if (last > 0) {
-            return nums.length;
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= 0) {
-                return i;
-            }
-        }
-
-        return nums.length;
-    }
-
     public static void main(String[] args) {
-        System.out.println(new MissingNumber_268().missingNumber(new int[]{1, 2, 0}));
-        System.out.println(new MissingNumber_268().missingNumber(new int[]{2, 0}));
-        System.out.println(new MissingNumber_268().missingNumber(new int[]{3, 0, 1}));
-        System.out.println(new MissingNumber_268().missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+        System.out.println(new MissingNumber_268().missingNumber(new int[]{3, 0, 1})); // 2
+        System.out.println(new MissingNumber_268().missingNumber(new int[]{1, 2, 0})); // 3
+        System.out.println(new MissingNumber_268().missingNumber(new int[]{1, 2})); // 0
+        System.out.println(new MissingNumber_268().missingNumber(new int[]{2, 0})); // 1
+        System.out.println(new MissingNumber_268().missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1})); // 8
     }
 }
