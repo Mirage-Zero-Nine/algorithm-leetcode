37fcd71f73ceeca2d03e9503f49d1e3eb72b9f1b
Null <borismirage00@gmail.com>
2021-10-24 22:37:06 -0700

    solution: add a standard DP and an space-optimized solution

--- src/Solution/DynamicProgramming/MaxProfit_121.java
+++ src/Solution/DynamicProgramming/MaxProfit_121.java
@@ -1,7 +1,7 @@
 package Solution.DynamicProgramming;
 
 /**
- * Say you have an array for which the ith element is the price of a given stock on day i.
+ * You have an array for which the ith element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock)
  * Design an algorithm to find the maximum profit.
  * Note that you cannot sell a stock before you buy one.
@@ -13,8 +13,10 @@ package Solution.DynamicProgramming;
 
 public class MaxProfit_121 {
     /**
-     * Dynamic programming.
-     * Similar to max subarray problem.
+     * Dynamic programming. Similar to max subarray problem.
+     * There is only one "buy". Hence, the cost of buy is -profit[i].
+     * And the profit is -profit[j] + profit[i], where 0 <= j < i, which is finding the min profit[j] from 0 to i - 1.
+     * Hence, the translation is dp[i] = Math.max(dp[i - 1], prices[i] - minFrom0Toi).
      *
      * @param prices int array
      * @return max "profit"
@@ -26,15 +28,39 @@ public class MaxProfit_121 {
             return 0;
         }
 
-        int n = prices.length, minBuy = prices[0];     // min buy price
-        int[] dp = new int[n];
+        int maxProfit = 0, previousMax = 0, minCost = prices[0];
 
-        for (int i = 1; i < n; i++) {
-            minBuy = Math.min(minBuy, prices[i]);
-            dp[i] = Math.max(dp[i - 1], prices[i] - minBuy);       // min buy price and max sell price
+        for (int i = 1; i < prices.length; i++) {
+            minCost = Math.min(minCost, prices[i]);
+            maxProfit = Math.max(previousMax, prices[i] - minCost);
+            previousMax = maxProfit;
         }
 
-        return dp[n - 1];
+        return maxProfit;
+    }
+
+    /**
+     * Standard DP solution.
+     *
+     * @param prices given array
+     * @return max "profit"
+     */
+    public int maxProfitStandardDP(int[] prices) {
+
+        /* Corner case */
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int[] dp = new int[prices.length];
+        int minCost = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            minCost = Math.min(minCost, prices[i]);
+            dp[i] = Math.max(dp[i - 1], prices[i] - minCost);
+        }
+
+        return dp[dp.length - 1];
     }
 
     /**
