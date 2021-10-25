170a1cf531e343389c719e45971e8f277ecaee11
Null <borismirage00@gmail.com>
2021-10-24 22:51:21 -0700

    fix: incorrect problem description

--- src/Solution/DynamicProgramming/MaxProfit_122.java
+++ src/Solution/DynamicProgramming/MaxProfit_122.java
@@ -1,11 +1,11 @@
 package Solution.DynamicProgramming;
 
 /**
- * Example 1:
- * Input: [7,1,5,3,6,4]
- * Output: 7
- * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
- * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+ * On each day, you may decide to buy and/or sell the stock.
+ * You can only hold at most one share of the stock at any time.
+ * However, you can buy it then immediately sell it on the same day.
+ * Find and return the maximum profit you can achieve.
  *
  * @author BorisMirage
  * Time: 2019/01/22 18:44
@@ -48,6 +48,7 @@ public class MaxProfit_122 {
 
     /**
      * Greedy.
+     * No limit on transaction times, so directly sum up the increase in all the increasing subarray of given array.
      *
      * @param prices given array
      * @return max profit
