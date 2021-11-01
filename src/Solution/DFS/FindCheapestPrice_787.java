8cbb4ac11435667862b42930c8af31a85bf46826
Null <shine_rappers.0e@icloud.com>
2021-10-31 21:22:47 -0700

    fix: avoid TLE

--- src/Solution/DFS/FindCheapestPrice_787.java
+++ src/Solution/DFS/FindCheapestPrice_787.java
@@ -15,43 +15,64 @@ import java.util.*;
 public class FindCheapestPrice_787 {
     /**
      * BFS with priority queue.
-     * Use a min heap to maintain cheapest price.
+     * Use a min heap to maintain the cheapest price.
      * Each time, poll out the cheapest element from heap and find all available next stop.
      * Add all next available stop to the heap and keep this process, until destination is reached.
+     * To avoid cycling in the graph, when current city was visited with fewer stops, skip it.
      *
      * @param n       n cities
      * @param flights flight lists with departure city and destination city and price
      * @param src     initially departure city
      * @param dst     destination city
-     * @param k       at most k stop city
+     * @param k       at most k stop
      * @return the cheapest price from src to dst with up to k stops, return -1 if no such path
      */
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> m = new HashMap<>();
+        Map<Integer, List<int[]>> map = new HashMap<>(); // departure city -> list of destination and price
 
         for (int[] f : flights) {
-            if (!m.containsKey(f[0])) {
-                m.put(f[0], new ArrayList<>());
+            if (!map.containsKey(f[0])) {
+                map.put(f[0], new ArrayList<>());
             }
 
-            m.get(f[0]).add(new int[]{f[1], f[2]});     // src -> dest, cost
+            map.get(f[0]).add(new int[]{f[1], f[2]});     // src -> dest, cost
         }
 
         Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
 
         pq.add(new int[]{0, src, k + 1});     // current cost, departure, destination
 
+        int[] stopsForCity = new int[n]; // save remaining available stops for each city
+        Arrays.fill(stopsForCity, -1);
+
         while (!pq.isEmpty()) {
             int[] currentMin = pq.poll();
             int cost = currentMin[0], departure = currentMin[1], remainingStop = currentMin[2];
 
-            if (departure == dst) {
+            if (departure == dst) {  // reaches the destination
                 return cost;
             }
-            if (remainingStop > 0 && m.containsKey(departure)) {
-                for (int[] f : m.get(departure)) {
-                    int next = f[0], nextCost = f[1];
-                    pq.add(new int[]{cost + nextCost, next, remainingStop - 1});
+
+            /*
+             * Modification of Dijkstra's algorithm: avoid cycling in graph.
+             * If stopsForCity[departure] == -1, then this city was not visited before.
+             * If stopsForCity[departure] >= remainingStop, it's actually a cycle.
+             * The top of the priority queue is the lowest cost available at previous city.
+             * If taking more steps but reached at same city, it's actually a cycle in the graph.
+             * For example: src = 1, dst = 4
+             * 1 -> 2: 100
+             * 2 -> 3: 50
+             * 3 -> 1: 10
+             * 3 -> 4: 100
+             * If the stops were not count, then it will loop in 1 -> 2 -> 3 -> 1, since 3 -> 1 cost 100, smaller than 400. */
+            if (stopsForCity[departure] == -1 || stopsForCity[departure] < remainingStop) {
+                stopsForCity[departure] = remainingStop;
+
+                if (remainingStop > 0 && map.containsKey(departure)) {
+                    for (int[] f : map.get(departure)) {
+                        int next = f[0], nextCost = f[1];
+                        pq.add(new int[]{cost + nextCost, next, remainingStop - 1});
+                    }
                 }
             }
         }
