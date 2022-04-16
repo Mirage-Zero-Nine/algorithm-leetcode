b64b8206878586be54401514b0bf8b147dabac54
Null <shine_rappers.0e@icloud.com>
2022-04-15 22:54:16 -0700

    doc: minor revision

--- src/Solution/Map/TwoSum_1.java
+++ src/Solution/Map/TwoSum_1.java
@@ -14,8 +14,9 @@ import java.util.HashMap;
 
 public class TwoSum_1 {
     /**
-     * Use a hash map to store the element in array, and value in hash map is the index of array.
-     * Iterate the array, if found an element that is equals to target - nums[i], then return the i and map value.
+     * Use a hash map to store the element in array.
+     * The key of the hash map is elements in the array, the value under each key is the element’s index.
+     * Traverse the array, if found an element that is equals to target - nums[i], then return the i and mapped index.
      * Otherwise, put the nums[i] and i into hash map.
      *
      * @param nums   given array
