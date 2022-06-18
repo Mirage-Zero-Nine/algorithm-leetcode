d3edece207eacd3e8a7f5765a4278fc370839595
Null <shine_rappers.0e@icloud.com>
2022-06-18 14:55:45 -0700

    test: add test

---
+++ src/test/java/solution/anagram/GroupAnagrams49Test.java
@@ -0,0 +1,51 @@
+package solution.anagram;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * @author BorisMirage
+ * Time: 2022/06/18 14:35
+ * Created with IntelliJ IDEA
+ */
+
+public class GroupAnagrams49Test {
+    private final GroupAnagrams_49 test = new GroupAnagrams_49();
+
+    @Test
+    public void test() {
+        String[] testArray = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> actual = test.groupAnagrams(testArray).stream().sorted(Comparator.comparingInt(List::size)).collect(Collectors.toList());
+        Iterator<Set<String>> expected = expectedResult().iterator();
+        actual.forEach(
+                list -> assertEquals(expected.next(), new HashSet<>(list))
+        );
+    }
+
+    @Test
+    public void testEmpty() {
+        List<List<String>> actual = test.groupAnagrams(new String[]{});
+        List<List<String>> expected = new ArrayList<>();
+        assertEquals(expected, actual);
+    }
+
+    private List<Set<String>> expectedResult() {
+        return Lists.newArrayList(
+                Sets.newHashSet("bat"),
+                Sets.newHashSet("tan", "nat"),
+                Sets.newHashSet("ate", "eat", "tea")
+        );
+    }
+}
