8b711cb6c88046c773bdd1f38912be5298118aaa
Null <shine_rappers.0e@icloud.com>
2022-06-19 20:17:44 -0700

    util: add 2d list parser

--- src/main/java/library/util/ListParser.java
+++ src/main/java/library/util/ListParser.java
@@ -1,8 +1,11 @@
 package library.util;
 
 import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -14,6 +17,8 @@ import java.util.stream.Collectors;
  */
 
 public class ListParser {
+    private static final Pattern pattern = Pattern.compile("\\[[\\s*\\d+,]+[\\s*\\d+]+]");
+
     /**
      * Parse 1d integer list string to list.
      *
@@ -33,5 +38,15 @@ public class ListParser {
                     return Integer.parseInt(value.trim());
                 })
                 .collect(Collectors.toList());
+    }
+
+    public static List<List<Integer>> parse2dList(String listString) {
+        Matcher matcher = pattern.matcher(listString);
+        List<List<Integer>> output = new ArrayList<>();
+        while (matcher.find()) {
+            output.add(parseList(matcher.group()));
+        }
+
+        return output;
     }
 }
