ab92fb82729666ff4341e46ac68218e0d2917a3e
Null <shine_rappers.0e@icloud.com>
2023-04-15 23:58:18 -0700

    solution: add unit test

--- src/main/java/solution/dfs/PathSum_437.java
+++ src/main/java/solution/dfs/PathSum_437.java
@@ -3,6 +3,7 @@ package solution.dfs;
 import library.tree.binarytree.TreeNode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * You are given a binary tree in which each node contains an integer value.
@@ -17,7 +18,9 @@ import java.util.HashMap;
 
 public class PathSum_437 {
     /**
-     * DFS post order traversal. Not only current node, but also all child nodes.
+     * DFS post order traversal. Starts from leaf node.
+     * For each node, heck if current node is the end of the path (node value equals to the sum remainder).
+     * Also, for each node, search each child node with original target sum (since path starts at each node).
      *
      * @param root root node
      * @param sum  target sum
@@ -32,17 +35,17 @@ public class PathSum_437 {
     }
 
     /**
-     * DFS recursion.
+     * DFS postorder traverse.
      * Path contains:
      * 1. path from previous node to current node
      * 2. path start from current node to left child node.
      * 3. path start from current node to right child node.
      *
      * @param n   current node
-     * @param sum given sum
+     * @param sum given sum (use <code>long</code> to avoid overflow)
      * @return number of path equal to given int until current node
      */
-    private int dfs(TreeNode n, int sum) {
+    private int dfs(TreeNode n, long sum) {
         if (n == null) {
             return 0;
         }
@@ -52,15 +55,33 @@ public class PathSum_437 {
 
     private int count = 0;
 
-    public int DFSWithMap(TreeNode root, int sum) {
-        HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
-        prefixSumCount.put(0, 1);
+    /**
+     * DFS with hash map to avoid duplicated search in child node.
+     *
+     * @param root root node
+     * @param sum  target sum
+     * @return number of path that sum equals given int
+     */
+    public int pathSumWithHashMap(TreeNode root, int sum) {
+        Map<Long, Integer> prefixSumCount = new HashMap<>(); // map to store path count under each prefix sum
+        prefixSumCount.put(0L, 1);
         dfs(root, 0, sum, prefixSumCount);
         return count;
     }
 
-    public void dfs(TreeNode node, int pathSum, int target, HashMap<Integer, Integer> map) {
-        if (node == null) return;
+    /**
+     * Postorder traversal with hash map.
+     *
+     * @param node    current tree node
+     * @param pathSum current path sum
+     * @param target  target sum
+     * @param map     hash map with path count
+     */
+    public void dfs(TreeNode node, long pathSum, int target, Map<Long, Integer> map) {
+        if (node == null) {
+            return;
+        }
+
         pathSum += node.val;
         if (map.containsKey(pathSum - target)) {
             count += map.get(pathSum - target);
@@ -70,7 +91,6 @@ public class PathSum_437 {
         dfs(node.left, pathSum, target, map);
         dfs(node.right, pathSum, target, map);
 
-        map.put(pathSum, map.get(pathSum) - 1); // setback!!!!!!!!!
-
+        map.put(pathSum, map.get(pathSum) - 1);
     }
 }
