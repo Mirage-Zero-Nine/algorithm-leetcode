1e735bdffa3b825fa55cf37fd086eca8ceb9ca75
Null <shine_rappers.0e@icloud.com>
2025-05-07 16:42:10 -0700

    test: add more unit tests

--- src/test/java/solution/bfs/LadderLength127Test.java
+++ src/test/java/solution/bfs/LadderLength127Test.java
@@ -35,5 +35,47 @@ public class LadderLength127Test {
     public void testInvalidInput() {
         List<String> wordList = Lists.newArrayList();
         assertEquals(0, test.ladderLength("a", "c", wordList));
+        wordList = Lists.newArrayList();
+        assertEquals(0, test.ladderLength("hit", "cog", wordList));
+        wordList = Lists.newArrayList("hot", "dot", "dog", "lot", "log", "cog", "hit");
+        assertEquals(0, test.ladderLength("", "cog", wordList));
+        wordList = Lists.newArrayList("hot", "dot", "dog", "lot", "log", "cog", "hit");
+        assertEquals(0, test.ladderLength("aaa", "", wordList));
+    }
+
+    @Test
+    public void testSingleLetterWords() {
+        List<String> wordList = Lists.newArrayList("a", "b", "c");
+        assertEquals(2, test.ladderLength("a", "c", wordList));
+    }
+
+    @Test
+    public void testBeginEqualsEnd() {
+        List<String> wordList = Lists.newArrayList("same", "lame", "lime");
+        assertEquals(1, test.ladderLength("same", "same", wordList));
+    }
+
+    @Test
+    public void testOneLetterChangeEach() {
+        List<String> wordList = Lists.newArrayList("hot", "dot", "dog", "cog");
+        assertEquals(5, test.ladderLength("hit", "cog", wordList));
+    }
+
+    @Test
+    public void testCaseSensitiveFail() {
+        List<String> wordList = Lists.newArrayList("Hot", "Dot", "Dog", "Lot", "Log", "Cog");
+        assertEquals(0, test.ladderLength("hit", "cog", wordList));
+    }
+
+    @Test
+    public void testDisconnectedGraph() {
+        List<String> wordList = Lists.newArrayList("aaa", "aab", "abb", "bbb", "ccc");
+        assertEquals(0, test.ladderLength("aaa", "ccc", wordList));
+    }
+
+    @Test
+    public void testCycleAvoidance() {
+        List<String> wordList = Lists.newArrayList("hot", "dot", "dog", "lot", "log", "cog", "hit");
+        assertEquals(5, test.ladderLength("hit", "cog", wordList));
     }
 }
