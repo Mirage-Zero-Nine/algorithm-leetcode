72aadfb3d9790dda1615c25a2357685419818045
Null <shine_rappers.0e@icloud.com>
2025-05-07 23:33:04 -0700

    test: add unit tests

--- src/main/java/solution/unionfind/CountComponents_323.java
+++ src/main/java/solution/unionfind/CountComponents_323.java
@@ -14,43 +14,60 @@ package solution.unionfind;
 
 public class CountComponents_323 {
     /**
-     * Union find.
-     * Initially, consider all nodes as individual nodes.
-     * Then traverse all given edges. Union two vertices that connected by edge.
+     * Counts the number of connected components in an undirected graph using the Union-Find data structure.
+     * A connected component is a subgraph where all nodes are connected to each other via some path.
      *
-     * @param n     # of nodes
-     * @param edges edges connect nodes
-     * @return # of connected components in an undirected graph
+     * @param n     the number of nodes in the graph
+     * @param edges a 2D array where each element is an edge represented by a pair of integers (u, v)
+     * @return the number of connected components in the graph
      */
     public int countComponents(int n, int[][] edges) {
-        int[] union = new int[n];
 
-        for (int i = 0; i < union.length; i++) {
-            union[i] = i;       // count each node as individual node
+        // corner cases
+        if (n == 0) {
+            return 0;
         }
-        for (int[] e : edges) {
-            int r1 = find(e[0], union);
-            int r2 = find(e[1], union);
-            if (r1 != r2) {
-                union[r1] = union[r2];      // union two nodes as connected
+        if (edges == null || edges.length == 0 || edges[0].length == 0) {
+            return n;
+        }
+
+        int[] root = new int[n];
+        for (int i = 0; i < n; i++) {
+            root[i] = i;
+        }
+
+        for (int[] edge : edges) {
+
+            // find the root for both of the elements
+            int root1 = find(edge[0], root), root2 = find(edge[1], root);
+
+            // if they are not under the same root, connect them
+            if (root1 != root2) {
+                root[root1] = root[root2];
                 n--;
             }
         }
+
         return n;
     }
 
     /**
-     * Find operation.
+     * Finds the root of a given node with path compression.
+     * When the value in root array equals the current value, the deepest root is found for this value.
      *
-     * @param i   given node
-     * @param arr array of union situation
-     * @return root of current node
+     * @param i    the node whose root is to be found
+     * @param root the parent array used for Union-Find operations
+     * @return the root of the given value
      */
-    private int find(int i, int[] arr) {
-        while (i != arr[i]) {
-            arr[i] = arr[arr[i]];       // update root if current node is not root
-            i = arr[i];
+    private int find(int i, int[] root) {
+
+        // the loop walks up the parent chain until it reaches the root (i == root[i]).
+        while (i != root[i]) {
+            // updates root[i] to point to its grandparent (root[root[i]]), until reaches the actual root
+            root[i] = root[root[i]]; // compress path
+            i = root[i];
         }
+
         return i;
     }
 }
